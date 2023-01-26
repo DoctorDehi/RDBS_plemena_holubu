df9b98af41c40f9827425d2e6ae88be7ceec9971
doctordehi <doctordehi@gmail.com>
2023-01-26 12:37:30 +0100

    static files, seznam plemen, zakladní barchart, detail plemene, vlajecky

--- webapp/app.py
+++ webapp/app.py
@@ -1,11 +1,53 @@
-from flask import Flask
+import os
+from flask import Flask, render_template, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func, desc, text
+from models import *
+
 
 app = Flask(__name__)
 
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://test:testovaci@localhost:5432/cznp_holub'
+
+db = SQLAlchemy(app)
+
+
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    plemena = db.session.query(Plemeno).all()
+    return render_template('index.html', plemena=plemena)
+
+
+@app.route('/grafy/pocty-skupiny')
+def graph_count_groups():
+    subquery = (db.select(
+            Skupina.nazev.label("nazev"),
+            func.count(Plemeno.id_ple).label("pocet"),
+        ).join(Plemeno.skupina_r).group_by(Plemeno.skupina, Skupina.nazev)).subquery()
+
+    result = db.session.execute(db.select(
+        subquery.c.nazev, subquery.c.pocet, func.rank().over(order_by=desc(text("pocet")))
+    )).all()
+
+    labels = []
+    values = []
+    for i in result:
+        labels.append(i[0])
+        values.append(i[1])
+    return render_template('bar_chart.html', labels=labels, values=values)
+
+@app.route("/plemena/<int:id_ple>")
+def plemeno_detail(id_ple):
+    plemeno = db.session.query(Plemeno).filter(Plemeno.id_ple==id_ple).one()
+    return render_template("detail_plemene.html", plemeno=plemeno)
+
+
+@app.route('/test')
+def test():
+    result = db.session.execute(db.select(func.count(Plemeno.id_ple), Skupina.nazev).join(Skupina).group_by(Plemeno.skupina, Skupina.nazev)).all()
+    return jsonify(str(result))
 
 
 if __name__ == '__main__':
--- webapp/models.py
+++ webapp/models.py
@@ -1,32 +1,39 @@
-from sqlalchemy import Column
-from sqlalchemy import ForeignKey
-from sqlalchemy import Integer
-from sqlalchemy import String
+from sqlalchemy import Table, Column, ForeignKey
+from sqlalchemy import Integer, String, Text
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import relationship
 
+
 Base = declarative_base()
+
+
+plemena_razy = Table(
+    "plemena_razy",
+    Base.metadata,
+    Column("ple", ForeignKey("plemena.id_ple"), primary_key=True),
+    Column("raz", ForeignKey("barevne_razy.id_raz"), primary_key=True),
+)
 
 
 class Plemeno(Base):
     __tablename__ = "plemena"
 
     id_ple = Column(Integer, primary_key=True)
-    nazev = Column(String(30))
-    sk_nazev = Column(String(30))
-    de_nazev = Column(String(30))
-    uk_nazev = Column(String(30))
-    skupina = Column(Integer, ForeignKey("skupina.id_sku"), nullable=False)
-    velikost = Column()
-    misto_puvodu = Column()
-    krouzek = Column()
-    strucny_popis = Column()
+    nazev = Column(String(50), nullable=False)
+    sk_nazev = Column(String(50))
+    de_nazev = Column(String(50))
+    uk_nazev = Column(String(50))
+    skupina = Column(Integer, ForeignKey("skupiny.id_sku"), nullable=False)
+    velikost = Column(Integer, ForeignKey("velikosti.id_vel"), nullable=False)
+    misto_puvodu = Column(Integer, ForeignKey("mista_puvodu.id_mis"))
+    krouzek = Column(Integer, nullable=False)
+    strucny_popis = Column(Text)
 
     skupina_r = relationship("Skupina", back_populates="plemena")
+    velikost_r = relationship("Velikost", back_populates="plemena")
+    misto_puvodu_r = relationship("MistoPuvodu", back_populates="plemena")
 
-    addresses = relationship(
-        "Address", back_populates="user", cascade="all, delete-orphan"
-    )
+    razy = relationship("BarevnyRaz", secondary=plemena_razy)
 
     def __repr__(self):
         return f"Plemeno(id={self.id_ple!r}, name={self.nazev!r})"
@@ -36,8 +43,49 @@ class Skupina(Base):
     __tablename__ = "skupiny"
 
     id_sku = Column(Integer, primary_key=True)
-    nazev = Column(String, nullable=False)
+    nazev = Column(String(50), nullable=False)
+
     plemena = relationship("Plemeno", back_populates="skupina_r")
 
     def __repr__(self):
         return f"Skupina(id={self.id_sku!r}, nazev={self.nazev!r})"
+
+
+class MistoPuvodu(Base):
+    __tablename__ = "mista_puvodu"
+
+    id_mis = Column(Integer, primary_key=True)
+    nazev = Column(String(50), nullable=False)
+
+    plemena = relationship("Plemeno", back_populates="misto_puvodu_r")
+
+
+class Velikost(Base):
+    __tablename__ = "velikosti"
+
+    id_vel = Column(Integer, primary_key=True)
+    nazev = Column(String(50), nullable=False)
+
+    plemena = relationship("Plemeno", back_populates="velikost_r")
+
+
+class Barva(Base):
+    __tablename__ = "barvy"
+
+    id_bar = Column(Integer, primary_key=True)
+    nazev = Column(String(50), nullable=False)
+
+
+class Kresba(Base):
+    __tablename__ = "kresby"
+
+    id_kre = Column(Integer, primary_key=True)
+    nazev = Column(String(50), nullable=False)
+
+
+class BarevnyRaz(Base):
+    __tablename__ = "barevne_razy"
+
+    id_raz = Column(Integer, primary_key=True)
+    bar = Column(Integer, ForeignKey("barvy.id_bar"), nullable=False)
+    kre = Column(Integer, ForeignKey("kresby.id_kre"), nullable=False)
